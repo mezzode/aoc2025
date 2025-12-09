5088a430163c16d868c8ba4c2036a0b32c913b86
mezzode <mezzode@users.noreply.github.com>
2025-12-08 16:56:26 -0800

    Fix solver for Day 3 Part 1

--- src/day03.rs
+++ src/day03.rs
@@ -10,49 +10,35 @@ pub fn part1(input: impl BufRead, verbose: bool) -> Result<String, Box<dyn std::
 
 fn max_bank_joltage(bank: String, verbose: bool) -> Result<u32, Box<dyn std::error::Error>> {
     let mut joltage = 0u32;
-    let mut curr_first = None;
-    let mut curr_second = None;
+    let mut biggest_prev_batt = None;
 
     if verbose {
         println!("Bank: {}", bank);
     }
 
     for battery in bank.chars() {
-        // let battery = battery.to_digit(10)?;
         if verbose {
             println!(
-                "First: {:?}, Second: {:?}, Joltage: {}, Battery: {}",
-                curr_first, curr_second, joltage, battery
+                "Biggest prev batt: {:?}, Joltage: {}, Battery: {}",
+                biggest_prev_batt, joltage, battery
             );
         }
-        if curr_first == None {
-            curr_first = Some(battery);
+        if biggest_prev_batt == None {
+            biggest_prev_batt = Some(battery);
             continue;
         }
-        if curr_second == None {
-            curr_second = Some(battery);
-            let potential_joltage =
-                format!("{}{}", curr_first.ok_or("Invalid")?, battery,).parse()?;
-            if potential_joltage > joltage {
-                joltage = potential_joltage;
-            }
-            continue;
-        }
-        if battery > curr_second.ok_or("Invalid")? {
-            curr_second = Some(battery);
-        }
+
         let potential_joltage = format!(
             "{}{}",
-            curr_first.ok_or("Invalid")?,
-            curr_second.ok_or("Invalid")?
+            biggest_prev_batt.ok_or("Invalid")?,
+            battery,
         )
         .parse()?;
         if potential_joltage > joltage {
             joltage = potential_joltage;
         }
-        if battery > curr_first.ok_or("Invalid")? {
-            curr_first = Some(battery);
-            curr_second = None;
+        if battery > biggest_prev_batt.ok_or("Invalid")? {
+            biggest_prev_batt = Some(battery);
         }
     }
     if verbose {
